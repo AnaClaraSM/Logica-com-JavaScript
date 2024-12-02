aca776683f991f89a70e0aacd80be31c4d384866
AnaClaraSM <itsanaclarasm.dev@gmail.com>
2024-12-02 19:18:49 -0300

    chapter 4

--- app.js
+++ app.js
@@ -1,26 +1,24 @@
 // Boas vindas
 alert("Boas-vindas ao jogo do Número Secreto!");
 
-let numeroSecreto = 4; //Armazena o n.s.
+let numeroSecreto = parseInt((Math.random() * 100) + 1); //Gera um número aleatório entre 1 e 100
 console.log("Número Secreto: " + numeroSecreto);
-
 let chute; //Armazena o número informado pelo usuário - inicia vazio
-
 let tentativas = 1; //Armazena as tentativas de acerto - inicia em 1
 
 
 // Enquanto o chute for diferente do n.s.
 while (chute != numeroSecreto) {
     // Solicita a entrada do usuário
-    chute = prompt("Digite um número de 1 a 10.");
+    chute = prompt("Digite um número de 1 a 100.");
 
     console.log(`Chute: ${chute}`);
     console.log("Comparação dos valores: " + (chute == numeroSecreto));
 
     // Se o chute for igual ao número secreto
     if (chute == numeroSecreto) {
-        // Alerta de acerto
-        alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa(s)!`);
+        // Interrompe o loop
+        break;
     }
     // Senão (Caso o chute não seja igual ao número secreto)
     else {
@@ -41,3 +39,7 @@ while (chute != numeroSecreto) {
         tentativas++;
     }
 }
+// Ao sair do loop -> Quando o chute for igual ao n.s.
+// Alerta de acerto
+let palavraTentativa = tentativas > 1 ? "tentativas" : "tentativa";
+alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}!`);
